10b8ed245ea08aac6d6b635fdbdd6709ced14191
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-11 07:47:14 +0200

    auth route

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
