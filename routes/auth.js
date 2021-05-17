8a067f915dd3eceea52cac79c0250bef159ab540
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-17 07:41:01 +0200

    auth middleware

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 const { json } = require('express');
 
 const User = require('../models/User');
@@ -11,8 +12,15 @@ const User = require('../models/User');
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  //res.send('Get logged in user');
+  try {
+    const user = await await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/auth
