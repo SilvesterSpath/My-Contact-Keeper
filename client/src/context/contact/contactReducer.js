3700e43b5372fc1399a375b12b552dd2e8fe0305
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-25 09:25:26 +0200

    delete

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -8,14 +8,21 @@ import {
   CLEAR_FILTER,
 } from '../types';
 
-export default (state, action) => {
+const contactReducer = (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
       };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter((i) => i.id !== action.payload),
+      };
     default:
       return state;
   }
 };
+
+export default contactReducer;
